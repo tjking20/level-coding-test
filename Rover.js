e3aa6f57bd202552cdab1c683aa28b646017590f
tjking20 <tjking20@outlook.com>
2017-07-02 22:46:03 -0400

    reconfigured original app js into reusable class, added error handling and recursion to  interface

---
+++ Rover.js
@@ -0,0 +1,104 @@
+class Rover {
+	constructor(gridCoords, deployCoords, directions){
+		//outter grid coordinates
+		this.gridCoords = gridCoords.trim().split(" "),//turns the input string into an array and trims white space
+		this.gridLength = parseInt(this.gridCoords[0]),//parseInt turns the individual strings into integers
+		this.gridHeight = parseInt(this.gridCoords[1]),
+
+		//rover deployment coordinates
+		this.deployCoords = deployCoords.trim().split(" "),
+		this.xPos = parseInt(this.deployCoords[0]),
+		this.yPos = parseInt(this.deployCoords[1]),
+
+		//directions and heading
+		this.directions = directions.trim().toUpperCase(),//ensures all incoming direcitons are capitalized
+		this.heading = this.directions[0]
+		
+	}
+
+	moveRover() {
+		//switch case uses the current heading and outputs different outcomes for eacg
+		switch (this.heading){
+			case "N":
+				return this.yPos ++
+				break;
+
+			case "E":
+				return this.xPos ++
+				break;
+
+			case "S":
+				return this.yPos --
+				break;
+
+			case "W":
+				return this.xPos --
+				break;
+		}
+	}
+
+	turnRover(rotation) {
+		//cardinalDirections lists all headings in order
+		//this allows us to determine which direction is left or right of current heading
+		const cardinalDirections = ["N", "E", "S", "W"];
+
+		//old direcition indicates the index of the heading to be changed
+		let oldIndex = cardinalDirections.indexOf(this.heading);
+
+		if (rotation == "L") {
+			let indexLeft = oldIndex -1;//changes the index to the element on the left
+			if (indexLeft == -1) indexLeft = 3;//rotates to the end of the array
+			
+			let turnLeft = cardinalDirections[indexLeft];//assigns new element based on changed index
+			this.heading = turnLeft;//sets new heading
+
+		} else if (rotation == "R") {
+			let indexRight = oldIndex +1;
+			if (indexRight == 4) indexRight = 0;
+
+			let turnRight = cardinalDirections[indexRight];
+			this.heading = turnRight; 
+		}
+	}
+
+	initiateRover(){
+		const roverInstructions = this.directions
+
+		//loops over the intructions (skipping the heading in the [0] index)
+		//allows us to execute functions for movement or turning
+		for(var i = 1; i < roverInstructions.length; i++){
+
+			if ((roverInstructions[i] == "R") || (roverInstructions[i] == "L")){
+				//executes the turnRover based on which direction is passed in
+				this.turnRover(roverInstructions[i]);
+
+			}else if(roverInstructions[i] == "M"){
+				//execute moveRover based on current heading
+				this.moveRover();
+
+			 }else{
+			 	//if node encounters a direction other than L, R, or M it will reject the input
+				console.log("Sorry that is not a valid input, please try again!");
+				return;
+
+			}
+		}
+
+		if((this.xPos <= this.gridLength) && (this.yPos <= this.gridHeight)){
+
+			console.log(`The new rover position is: ${this.xPos} ${this.yPos} ${this.heading}`)
+		
+		} else{
+		
+			console.log("I'm sorry, your rover has driven off the plateau.",
+				"You should probably run. Their gonna be pretty pissed about this.");
+		
+		}
+		
+	}
+}
+module.exports = Rover;
+// const timRover = new Rover("5 5 ", " 16 5   ", "   nlmlmlmlmm ")
+// timRover.initiateRover();
+
+
--- interface.js
+++ interface.js
@@ -1,47 +1,39 @@
-//user interface that will be put in last
-var readline = require('readline');
 
 
+launchInterface = () =>{
+	const Rover = require('./Rover.js');
+	const readline = require('readline');
 
-const rl = readline.createInterface({
-  message: console.log("Welcome to the Mars Rover Control Center! "),
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question('Please enter the outer right coordinates: ', (gridCoords) => {
-	// const inputOne = gridCoords;
-	rl.question('Please enter the deployment coordinates: ', (deployCoords) => {
-		console.log(gridCoords);
-		console.log(deployCoords);
-
-		rl.question('Please enter movement instructions for the Rover to follow: ', 
-			(instructions)=>{
-
-				console.log(gridCoords);
-				console.log(deployCoords);
-				console.log(instructions);
-				rl.close()
-			});
-		
+	const rl = readline.createInterface({
+	  message: console.log("Welcome to the Mars Rover Control Center! "),
+	  input: process.stdin,
+	  output: process.stdout
 	});
- 
-});
-// var rl = readline.createInterface({
-//     message: console.log("hello"),
-//     input: process.stdin,
-//     output: process.stdout
-// });
 
-// rl.prompt();
+	rl.question('Please enter the outer right coordinates: ', (gridCoords) => {
 
-// rl.on('line', function (cmd) {
+		rl.question('Please enter the deployment coordinates: ', (deployCoords) => {
 
-//     input.push(cmd);
-// });
+			rl.question('Please enter movement directions for the Rover to follow: ', 
+			(directions)=>{			
 
-// rl.on('close', function (cmd) {
+				let rover = new Rover(gridCoords, deployCoords, directions);//establish a new Rover module
+				rover.initiateRover(); //initiate rover calculations with data from rl questions
 
-//     console.log(input.join('\n'));
-//     process.exit(0);
-// });
+				rl.question('Would you like to launch again? Please enter Y or N: ', 
+				(answer)=>{
+
+					answer = answer.toUpperCase();//ensures that both lower and upper case y's are acceted
+
+					if(answer == "Y") launchInterface();//using recursion the interface can reload
+
+					else rl.close()
+
+				});
+			});
+		});
+	});
+}
+
+launchInterface();
+
